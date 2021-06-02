1172689b2bb896c0d9c2ff13f48ddade43067dae
Thales Macena <thalesmacena@gmail.com>
2021-06-02 10:06:09 -0300

    create springboot app

---
+++ src/main/java/br/com/example/todocrud/TodoCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.todocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoCrudApplication.class, args);
+	}
+
+}
